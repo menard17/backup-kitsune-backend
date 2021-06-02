f30eb98c4972eef30bcabb2913438ac0596e71a2
Akira <akira.kakar@gmail.com>
2021-06-02 19:15:07 +0900

    Add organization with doc-strings

--- src/adapters/fhir_store.py
+++ src/adapters/fhir_store.py
@@ -1,6 +1,8 @@
-import google.auth
 from fhir.resources import construct_fhir_element
+from fhir.resources.fhirabstractmodel import FHIRAbstractModel
+import google.auth
 from google.auth.transport import requests
+from fhir.resources.domainresource import DomainResource
 
 # This configuration is only for testing purpose only. Should be separated to
 # different configuration for dev/test/prod in the future.
@@ -12,51 +14,94 @@ fhir_configuration = {
     "FHIR_STORE": "phat-fhir-store-id",
 }
 
+
 # Gets credentials from the Cloud Run environment or local GOOGLE_APPLICATION_CREDENTIALS
 # See https://googleapis.dev/python/google-auth/latest/reference/google.auth.html#google.auth.default
-credentials, project_id = google.auth.default()
-
-scoped_credentials = credentials.with_scopes(
-    ["https://www.googleapis.com/auth/cloud-platform"]
+credentials, project_id = google.auth.default(
+    scopes=["https://www.googleapis.com/auth/cloud-platform"]
 )
 
 
-def get_resource(resource_type, resource_id):
-    """Retrieve a resource from FHIR store.
+class ResourceClient:
+    def __init__(self):
+        self._session = requests.AuthorizedSession(credentials)
 
-    The data retrieved from FHIR store is a JSON object, which will be converted
-    into an fhir.resources Python object, using Resource Factory Function.
+        self._url = "{}/projects/{}/locations/{}".format(
+            fhir_configuration.get("BASE_URL"),
+            fhir_configuration.get("PROJECT"),
+            fhir_configuration.get("LOCATION"),
+        )
 
-    :param resource_type: The FHIR resource type
-    :type resource_type: str
+        self._headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
 
-    :param resource_id: The FHIR resource identifier
-    :type resource_id: str
+    def get_resource(
+        self,
+        resource_uid: str,
+        resource: DomainResource,
+    ) -> FHIRAbstractModel:
+        """Retrieve a resource from FHIR store.
+        The data retrieved from FHIR store is a JSON object, which will be converted
+        into an FHIRAbstractModel Python object, using Resource Factory Function.
 
-    :rtype: Resource
-    """
+        :param resource_id: The FHIR resource identifier
+        :type resource_id: str
+        :param resource: The FHIR resource
+        :type resource: DomainResource
 
-    # Creates a requests Session object with the credentials.
-    session = requests.AuthorizedSession(scoped_credentials)
+        :rtype: FHIRAbstractModel
+        """
+        resource_path = "{}/datasets/{}/fhirStores/{}/fhir/{}/{}".format(
+            self._url,
+            fhir_configuration.get("DATASET"),
+            fhir_configuration.get("FHIR_STORE"),
+            resource.resource_type,
+            resource_uid,
+        )
 
-    url = "{}/projects/{}/locations/{}".format(
-        fhir_configuration.get("BASE_URL"),
-        fhir_configuration.get("PROJECT"),
-        fhir_configuration.get("LOCATION"),
-    )
+        response = self._session.get(resource_path, headers=self._headers)
+        response.raise_for_status()
 
-    resource_path = "{}/datasets/{}/fhirStores/{}/fhir/{}/{}".format(
-        url,
-        fhir_configuration.get("DATASET"),
-        fhir_configuration.get("FHIR_STORE"),
-        resource_type,
-        resource_id,
-    )
+        return construct_fhir_element(resource.resource_type, response.json())
 
-    # Sets required application/fhir+json header on the request
-    headers = {"Content-Type": "application/fhir+json;charset=utf-8"}
+    def get_resources(self, resource: DomainResource) -> DomainResource:
+        """Retrieve all resources with given type from FHIR store.
+        The data retrieved from FHIR store is a JSON object, which will be converted
+        into an FHIRAbstractModel Python object, using Resource Factory Function.
 
-    response = session.get(resource_path, headers=headers)
-    response.raise_for_status()
+        :param resource: The FHIR resource
+        :type resource: DomainResource
 
-    return construct_fhir_element(resource_type, response.json())
+        :rtype: FHIRAbstractModel
+        """
+        resource_path = "{}/datasets/{}/fhirStores/{}/fhir/{}".format(
+            self._url,
+            fhir_configuration.get("DATASET"),
+            fhir_configuration.get("FHIR_STORE"),
+            resource.resource_type,
+        )
+
+        response = self._session.get(resource_path, headers=self._headers)
+        response.raise_for_status()
+
+        return construct_fhir_element("Bundle", response.json())
+
+    def create_resource(self, resource: DomainResource) -> FHIRAbstractModel:
+        """Creates a resource with DomainResource. Returns newly create resource
+        in FHIRAbstractModel Python object.
+
+        :param resource: The FHIR resource
+        :type resource: DomainResource
+
+        :rtype: FHIRAbstractModel
+        """
+        resource_path = "{}/datasets/{}/fhirStores/{}/fhir/{}".format(
+            self._url,
+            fhir_configuration.get("DATASET"),
+            fhir_configuration.get("FHIR_STORE"),
+            resource.resource_type,
+        )
+
+        response = self._session.post(
+            resource_path, headers=self._headers, json=resource.dict()
+        )
+        return construct_fhir_element(resource.resource_type, response.json())
---
+++ src/blueprints/organizations.py
@@ -0,0 +1,59 @@
+from flask import Blueprint, request
+from adapters.fhir_store import ResourceClient
+from middleware import jwt_authenticated
+from fhir.resources.organization import Organization
+from fhir.resources.address import Address
+
+organization_blueprint = Blueprint(
+    "organizations", __name__, url_prefix="/organizations"
+)
+
+
+@organization_blueprint.route("/<organization_id>", methods=["GET"])
+@jwt_authenticated
+def get_organization(organization_id: str) -> dict:
+    """Returns details of a organization. Organization can be clincs, hospitals, and etc.
+    Have to get FHIR's UUID from UID bypass for test.
+
+    :param organization_id: uuid for organization
+    :type organization_id: str
+
+    :rtype: Dictionary
+    """
+    resourse_client = ResourceClient()
+    return resourse_client.get_resource(organization_id, Organization()).dict()
+
+
+@organization_blueprint.route("/", methods=["GET"])
+@jwt_authenticated
+def get_organizations() -> dict:
+    """Returns details of all organizations.
+    Have to get FHIR's UUID from UID bypass for test
+
+    :rtype: Dictionary
+    """
+    resourse_client = ResourceClient()
+    return resourse_client.get_resources(Organization()).dict()
+
+
+@organization_blueprint.route("/", methods=["POST"])
+@jwt_authenticated
+def create_organization() -> dict:
+    """Returns details of a organization created.
+    This creates a organization in fhir.
+    This does not check if there is a duplicate.
+    By default, address is set to Japan.
+
+    Example:
+        body: {"name": "example hospital"}
+
+    rtype: Dictionary
+    """
+    resourse_client = ResourceClient()
+    organization = Organization.parse_obj(request.get_json())
+    organization.active = True
+    organization.address = list()
+    address = Address.construct()
+    address.country = "Japan"
+    organization.address.append(address)
+    return resourse_client.create_resource(organization).dict()
