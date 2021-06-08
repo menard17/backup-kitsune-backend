8f112ddba248d1935600bcb0c53fc6878023cf9b
Akira <akira.kakar@gmail.com>
2021-06-08 19:02:13 +0900

    deleted test fhir

--- src/adapters/test_fhir_store.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from resource_client import ResourceClient
-
-
-def test_resource_client():
-    sessino_mock = pytest.mock()
-    resource = pytest.mock()
-    resource_client = ResourceClient(sessino_mock)
-    resource_client.get_srouce
