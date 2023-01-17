9842a2b2acc8de7a8777b669026eed80eb45ed23
Akira <akira.kakkar@umed.jp>
2023-01-17 15:25:33 +0900

    fix: update key of get_list_id (#237)

--- src/blueprints/config.py
+++ src/blueprints/config.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response
 
 from json_serialize import json_serial
 from utils.middleware import jwt_authenticated
@@ -12,18 +12,18 @@ config_blueprint = Blueprint("config", __name__, url_prefix="/config")
 @jwt_authenticated()
 @config_blueprint.route("/", methods=["GET"])
 def get_list_id() -> Response:
-    return ConfigController().get_list_id(request)
+    return ConfigController().get_list_id()
 
 
 class ConfigController:
-    def get_list_id(self, request) -> Response:
+    def get_list_id(self) -> Response:
         "This is a mitigation plan to have an env variable to contain list id."
         LIST_ID = os.getenv("LIST_ID")
 
         return Response(
             status=200,
             response=json.dumps(
-                {"patient_queue_list_id": LIST_ID},
+                {"patientQueueListId": LIST_ID},
                 default=json_serial,
             ),
         )
